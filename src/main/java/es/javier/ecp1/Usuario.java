22d50099a2fe325a202c7154ac9bd11402a456e3
molinar <jmolinaromero86@gmail.com>
2014-10-05 14:14:01 +0200

    release 4

--- src/main/java/es/javier/ecp1/Usuario.java
+++ src/main/java/es/javier/ecp1/Usuario.java
@@ -7,17 +7,21 @@ public class Usuario {
     private String apellidos;
 
     private int edad;
+    
+    private int dni;
 
-    public Usuario(String nombre, String apellidos, int edad) {
+    public Usuario(String nombre, String apellidos, int edad, int dni) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.edad = edad;
+        this.dni = dni;
     }
 
     public void mostrarDatos() {
         System.out.println("Nombre: " + this.nombre);
         System.out.println("Apellidos: " + this.apellidos);
-        System.out.println("Edad" + this.edad);
+        System.out.println("Edad: " + this.edad);
+        System.out.println("DNI: " + this.dni);
     }
 
     public String getNombre() {
