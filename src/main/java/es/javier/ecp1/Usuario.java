f079790229629a218bac988f6cf5302b18d1bc5e
molinar <jmolinaromero86@gmail.com>
2014-10-05 13:56:31 +0200

    develop 4

--- src/main/java/es/javier/ecp1/Usuario.java
+++ src/main/java/es/javier/ecp1/Usuario.java
@@ -8,15 +8,19 @@ public class Usuario {
 
     private int edad;
 
-    public Usuario(String nombre, String apellidos, int edad) {
+    private String sexo;
+
+    public Usuario(String nombre, String apellidos, int edad, String sexo) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.edad = edad;
+        this.sexo = sexo;
     }
 
     public void mostrarDatos() {
         System.out.println("Nombre: " + this.nombre);
         System.out.println("Apellidos: " + this.apellidos);
-        System.out.println("Edad" + this.edad);
+        System.out.println("Edad: " + this.edad);
+        System.out.println("Sexo: " + this.sexo);
     }
 }
